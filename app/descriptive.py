238406588373806a6b7c8d7b0248c395b59a3e1f
Luca Soto De Graeve <lucasotodegraeve@gmail.com>
2025-05-21 10:36:11 +0200

    Refactor descriptive parsing

--- app/descriptive.py
+++ app/descriptive.py
@@ -1,82 +1,80 @@
 from typing import Any, Literal
 
-import dateutil.parser
-from lxml import etree
-from lxml.etree import _Element
-from sippy.descriptive import Concept, Person, Place, QuantitativeValue, Role, Thing
+from sippy.descriptive import (
+    Concept,
+    Person,
+    Place,
+    QuantitativeValue,
+    Role,
+    Thing,
+)
 from sippy.utils import DateTime, EDTF_level1, Float, LangStr
 
+from app.dc_schema import DCSchema, DCTRole, Measurement, XMLLangStr
 from app.mets import METS
-from app.utils import (
-    ParseException,
-    xpath_element_list,
-    xpath_lang_str,
-    xpath_optional_element,
-    xpath_optional_lang_str,
-    xpath_optional_text,
-    xpath_text,
-    xpath_text_list,
-)
+from app.utils import ParseException
 
 
-def parse_role(
-    dcterms: _Element, role_type: Literal["creator", "publisher", "contributor"]
-) -> list[Role]:
+def to_sippy_lang_str(str: XMLLangStr | None) -> LangStr | None:
+    if str is None:
+        return None
+    return LangStr.codes(**str.content)
 
-    match role_type:
-        case "creator":
-            default_role_name = "Maker"
-        case "publisher":
-            default_role_name = "Publisher"
-        case "contributor":
-            default_role_name = "Bijdrager"
 
-    roles_xml = xpath_element_list(dcterms, f"schema:{role_type}") + xpath_element_list(
-        dcterms, f"dcterms:{role_type}"
+def to_sippy_role(
+    role: DCTRole, type: Literal["contributor", "creator", "publisher"]
+) -> Role:
+
+    if role.birth_date or role.death_date:
+        member = Person(
+            name=LangStr(nl=role.name),
+            birth_date=EDTF_level1(value=role.birth_date) if role.birth_date else None,
+            death_date=EDTF_level1(value=role.death_date) if role.death_date else None,
+        )
+    else:
+        member = Thing(name=LangStr(nl=role.name))
+
+    if role.role_name is None:
+        match type:
+            case "contributor":
+                role_name = "Bijdrager"
+            case "publisher":
+                role_name = "Publisher"
+            case "creator":
+                role_name = "Maker"
+    else:
+        role_name = role.role_name
+
+    return Role(
+        role_name=role_name,
+        creator=member if type == "creator" else None,
+        publisher=member if type == "publisher" else None,
+        contributor=member if type == "contributor" else None,
     )
 
-    roles = []
-    for role_xml in roles_xml:
-        role_name = xpath_optional_text(role_xml, "@schema:roleName")
-        name = xpath_text(role_xml, "schema:name/text()")
-        member = Thing(
-            name=LangStr(nl=name),
-        )
 
-        birth_date = xpath_optional_text(role_xml, "schema:birthDate")
-        death_date = xpath_optional_text(role_xml, "schema:deathDate")
+def to_sippy_quantitive_value(
+    measurement: Measurement | None,
+) -> QuantitativeValue | None:
 
-        if birth_date or death_date:
-            member = Person(
-                name=LangStr(nl=name),
-                birth_date=EDTF_level1(value=birth_date) if birth_date else None,
-                death_date=EDTF_level1(value=death_date) if death_date else None,
-            )
+    if measurement is None:
+        return None
 
-        roles.append(
-            Role(
-                role_name=role_name if role_name else default_role_name,
-                publisher=member if role_type == "publisher" else None,
-                creator=member if role_type == "creator" else None,
-                contributor=member if role_type == "contributor" else None,
-            )
-        )
-
-    return roles
-
-
-def unit_text_to_code(text: str) -> str:
-    match text:
+    match measurement.unit_text:
         case "mm":
-            return "MMT"
+            unit_code = "MMT"
         case "cm":
-            return "CMT"
+            unit_code = "CMT"
         case "m":
-            return "MTR"
+            unit_code = "MTR"
         case "kg":
-            return "KGM"
+            unit_code = "KGM"
 
-    raise ParseException(f"Unknown unit text: {text}")
+    return QuantitativeValue(
+        value=Float(value=measurement.value),
+        unit_text=measurement.unit_text,
+        unit_code=unit_code,
+    )
 
 
 def parse_descriptive(mets: METS) -> dict[str, Any]:
@@ -84,108 +82,49 @@ def parse_descriptive(mets: METS) -> dict[str, Any]:
     if mets.descriptive_metadata is None:
         raise ParseException("Package must have descriptive metdata")
 
-    dcterms = etree.parse(mets.descriptive_metadata).getroot()
-    alternative = xpath_optional_lang_str(dcterms, "dcterms:alternative")
-
-    available = xpath_optional_text(dcterms, "dcterms:available/text()")
-    available = dateutil.parser.parse(available) if available else None
-
-    issued = xpath_optional_text(dcterms, "dcterms:issued/text()")
-
-    spatial = xpath_text_list(dcterms, "dcterms:spatial")
-    spatial = [Place(name=LangStr(nl=s)) for s in spatial]
-
-    copyright_holder = xpath_optional_text(dcterms, "dcterms:rightsHolder/text()")
-
-    height_value = xpath_optional_text(dcterms, "schema:height/schema:value/text()")
-    height_unit_text = xpath_optional_text(
-        dcterms, "schema:height/schema:unitText/text()"
-    )
-    depth_value = xpath_optional_text(dcterms, "schema:depth/schema:value/text()")
-    depth_unit_text = xpath_optional_text(
-        dcterms, "schema:depth/schema:unitText/text()"
-    )
-    width_value = xpath_optional_text(dcterms, "schema:width/schema:value/text()")
-    width_unit_text = xpath_optional_text(
-        dcterms, "schema:width/schema:unitText/text()"
-    )
-    weight_value = xpath_optional_text(dcterms, "schema:weight/schema:value/text()")
-    weight_unit_text = xpath_optional_text(
-        dcterms, "schema:weight/schema:unitText/text()"
-    )
-
+    dc_schema = DCSchema.from_xml(str(mets.descriptive_metadata))
     return {
-        "name": xpath_lang_str(dcterms, "dcterms:title"),
-        "alternative_name": [alternative] if alternative else [],
-        # TODO: dcterms:extend
-        "available": DateTime(value=available) if available else None,
-        "description": xpath_lang_str(dcterms, "dcterms:description"),
-        "abstract": xpath_optional_lang_str(dcterms, "dcterms:abstract"),
-        "date_created": EDTF_level1(
-            value=xpath_text(dcterms, "dcterms:created/text()")
+        "name": to_sippy_lang_str(dc_schema.title),
+        "alternative_name": [to_sippy_lang_str(dc_schema.alternative)],
+        # # TODO: dcterms:extend
+        "available": (
+            DateTime(value=dc_schema.available) if dc_schema.available else None
         ),
-        "date_published": EDTF_level1(value=issued) if issued else None,
-        "publisher": parse_role(dcterms, "publisher"),
-        "contributor": parse_role(dcterms, "contributor"),
-        "creator": parse_role(dcterms, "creator"),
-        "spatial": spatial,
-        # "temporal": TODO
-        "keywords": (
-            [keywords]
-            if (keywords := xpath_optional_lang_str(dcterms, "dcterms:subject"))
-            else []
+        "description": to_sippy_lang_str(dc_schema.description),
+        "abstract": to_sippy_lang_str(dc_schema.abstract),
+        "date_created": EDTF_level1(value=dc_schema.created),
+        "date_published": (
+            EDTF_level1(value=dc_schema.issued) if dc_schema.issued else None
         ),
-        "in_language": xpath_text_list(dcterms, "dcterms:language"),
-        # TODO: is this simple mapping for licenses ok?
+        "publisher": [
+            to_sippy_role(publisher, "publisher") for publisher in dc_schema.publisher
+        ],
+        "creator": [to_sippy_role(creator, "creator") for creator in dc_schema.creator],
+        "contributor": [
+            to_sippy_role(contributor, "contributor")
+            for contributor in dc_schema.contributor
+        ],
+        "spatial": [Place(name=LangStr(nl=s)) for s in dc_schema.spatial],
+        # # "temporal": TODO
+        "keywords": [to_sippy_lang_str(dc_schema.subject)] if dc_schema.subject else [],
+        "in_language": dc_schema.language,
+        # # TODO: is this simple mapping for licenses ok?
         "license": [
             Concept(id="https://data.hetarchief.be/id/license/" + l)
-            for l in xpath_text_list(dcterms, "dcterms:license/text()")
+            for l in dc_schema.license
         ],
         "copyright_holder": (
-            [Thing(name=LangStr(nl=copyright_holder))] if (copyright_holder) else []
-        ),
-        "rights": (
-            [LangStr(nl=rights)]
-            if (rights := xpath_optional_text(dcterms, "dcterms:rights/text()"))
+            [Thing(name=LangStr(nl=dc_schema.rights_holder))]
+            if dc_schema.rights_holder
             else []
         ),
-        # "type"
-        "height": (
-            QuantitativeValue(
-                value=Float(value=float(height_value)),
-                unit_code=unit_text_to_code(height_unit_text),
-                unit_text=height_unit_text,
-            )
-            if height_value and height_unit_text
-            else None
-        ),
-        "width": (
-            QuantitativeValue(
-                value=Float(value=float(width_value)),
-                unit_code=unit_text_to_code(width_unit_text),
-                unit_text=width_unit_text,
-            )
-            if width_value and width_unit_text
-            else None
-        ),
-        "depth": (
-            QuantitativeValue(
-                value=Float(value=float(depth_value)),
-                unit_code=unit_text_to_code(depth_unit_text),
-                unit_text=depth_unit_text,
-            )
-            if depth_value and depth_unit_text
-            else None
-        ),
-        "weight": (
-            QuantitativeValue(
-                value=Float(value=float(weight_value)),
-                unit_code=unit_text_to_code(weight_unit_text),
-                unit_text=weight_unit_text,
-            )
-            if weight_value and weight_unit_text
-            else None
-        ),
+        "rights": ([to_sippy_lang_str(dc_schema.rights)] if dc_schema.rights else []),
+        # TODO: moet type naar format gemapped worden? format heeft `1..1` en type `0..1`
+        # "format": dc_schema.type,
+        "height": to_sippy_quantitive_value(dc_schema.height),
+        "width": to_sippy_quantitive_value(dc_schema.width),
+        "depth": to_sippy_quantitive_value(dc_schema.depth),
+        "weight": to_sippy_quantitive_value(dc_schema.weight),
         # "art_medium":
         # "artform"
         # "schema_is_part_of"

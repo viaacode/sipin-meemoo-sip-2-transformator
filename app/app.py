7b99a5a7558dfc5ddef1204a20a62daaab553c0c
Luca Soto De Graeve <lucasotodegraeve@gmail.com>
2025-05-22 11:10:31 +0200

    Rename and clean up entry point for parsing

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from sippy.sip import IntellectualEntity
+from sippy.sip import SIP, IntellectualEntity
 from sippy.utils import Config, String
 from sippy.vocabulary import EntityClass
 from app.descriptive import parse_descriptive
@@ -8,18 +8,17 @@ from app.mets import parse_mets
 from app.preservation import PremisFiles
 
 
-PATH = "/home/luca/Projects/documentation/assets/sip_samples/2.1/film_standard_mkv/uuid-2746e598-75cd-47b5-9a3e-8df18e98bb95"
-
 Config.SET_FIELDS_EXPLICIT = False
 
 
-def parse_package():
+def parse_sip(path) -> SIP:
+    """
+    Parse a meemoo SIP given its root folder.
+    """
 
-    mets_path = Path(PATH).joinpath("METS.xml")
-
+    mets_path = Path(path).joinpath("METS.xml")
     package_mets = parse_mets(mets_path)
     premis_files = PremisFiles(package_mets)
-
     structural = premis_files.get_structural_info()
     descriptive = parse_descriptive(package_mets)
 
@@ -33,6 +32,8 @@ def parse_package():
 
     events = premis_files.parse_events()
 
-    from rich import print
-
-    print(events)
+    return SIP(
+        entity=ie,
+        events=events,
+        premis_agents=[],
+    )
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from app.app import parse_package
+from app.app import parse_sip
 
 
 if __name__ == "__main__":
-    parse_package()
+    PATH = "/home/luca/Projects/documentation/assets/sip_samples/2.1/film_standard_mkv/uuid-2746e598-75cd-47b5-9a3e-8df18e98bb95"
+    parse_sip(PATH)

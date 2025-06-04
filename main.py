dc7930250415efb16157e3e86b21c7dffa35687a
Luca Soto De Graeve <lucasotodegraeve@gmail.com>
2025-06-04 17:48:06 +0200

    Add simple tests to parse film and newspaper

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-from app.app import parse_sip
-
-
 if __name__ == "__main__":
-    PATH = "/home/luca/Projects/documentation/assets/sip_samples/2.1/film_standard_mkv/uuid-2746e598-75cd-47b5-9a3e-8df18e98bb95"
-    parse_sip(PATH)
+    raise NotImplementedError()

632f761527871a57fb199a872f0f7034f27186ff
Luca Soto De Graeve <lucasotodegraeve@gmail.com>
2025-06-25 12:24:12 +0200

    rebase: update eark import

--- app/v2_1/descriptive/mods.py
+++ app/v2_1/descriptive/mods.py
@@ -3,19 +3,19 @@ from itertools import chain
 from pathlib import Path
 from functools import partial
 
-from eark_models import mods
+from eark_models.mods.v3_7 import Mods
 import sippy
 
 from ..utils import ParseException
 
 
 def parse_mods(path: Path) -> partial[sippy.IntellectualEntity]:
-    desc = mods.MODS.from_xml(path)
+    desc = Mods.from_xml(path)
 
-    main_title_info = next(info for info in desc.titleInfos if info.type is None)
+    main_title_info = next(info for info in desc.title_infos if info.type is None)
     main_title = next(iter(main_title_info.titles))
 
-    date_created = next(chain(*[info.datesCreated for info in desc.originInfos]))
+    date_created = next(chain(*[info.dates_created for info in desc.origin_infos]))
     if date_created.encoding != "edtf":
         raise ParseException(
             "Date created must have attribute 'encoding' set to 'edtf'"
